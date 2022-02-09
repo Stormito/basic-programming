64c2ee5b1106bd1f9598614a146438ab69d72ee1
Storme <98614524+Stormito@users.noreply.github.com>
2022-02-08 22:18:40 -0300

    first class

---
+++ Personal data.py
@@ -0,0 +1,7 @@
+name = input('Write your name: ')
+age = int(input('How old are you? '))
+salary = float(input('How much money do you make per moth? '))
+
+print('----Personal data----')
+print(f'Name: {name}\nAge: {age}\nSalary: {salary}\n')
+print(f'How can we see, {name} is {age} years old and make ${salary} per moth')
---
+++ Write the name.py
@@ -0,0 +1,2 @@
+name = input('write your name: ')
+print(f'hello {name}, it\'s a pleasure to meet you')
